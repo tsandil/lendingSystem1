60273dbdfbbca889762c117d22f8f8a84125ff37
tsandil <tan.sandil44@gmail.com>
2021-10-03 12:38:33 +0545

    myacommit

---
+++ form/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Feedback
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'date', 'happy',)
+    list_filter = ( 'date',)
+    search_fields = ('details',)
+
+    class Meta:
+        model = Feedback
+
+
+admin.site.register(Feedback, FeedbackAdmin)
+
---
+++ form/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+
+
+class Feedback(models.Model):
+    customer_name = models.CharField(max_length=120)
+    email = models.EmailField()
+    details = models.TextField()
+    happy = models.BooleanField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer_name
